d5f7b1aa480fba4f1efb627af5d66c0ff43267d2
daikichi <yamadai.cell@gmail.com>
2025-01-09 21:57:16 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,6 +37,19 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
