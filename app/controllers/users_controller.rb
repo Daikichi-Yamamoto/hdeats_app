0ef4cd7878597f73529b59e5fd859aa5a2dfa2cb
daikichi <yamadai.cell@gmail.com>
2025-01-12 00:23:40 +0900

    ユーザーにログインを要求する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
   # ログイン済みユーザーかどうか確認
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "ログインしてください"
       redirect_to login_url, status: :see_other
     end
@@ -56,6 +57,6 @@ class UsersController < ApplicationController
   # 正しいユーザーかどうか確認
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless @user == current_user
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,37 @@
 module SessionsHelper
+
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
+    # セッションリプレイ攻撃から保護する
+    session[:session_token] = user.session_token
   end
 
-  # 永続的セッションのためにユーザーをデータベースに記憶する
+  # ユーザーを永続的セッションに保存する
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.encrypted[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # 現在のユーザーを返す（セッションまたはクッキーから取得）
   def current_user
+    # セッションからユーザーを確認
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id]) # signed cookie で user_id を取得
+    # セッションがない場合、クッキーから確認
+    elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token]) # remember_token を照合
+    # ユーザーが存在し、remember_tokenが一致すればログイン
+      if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
+  end
+
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
   end
 
   # ユーザーがログインしていれば true、その他なら false を返す
@@ -41,5 +51,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil   # 安全のため
+  end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
