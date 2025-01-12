fe0a709faf9b23bbdb28756a19a58d97655df21d
daikichi <yamadai.cell@gmail.com>
2025-01-12 12:00:54 +0900

    ユーザー編集機能完了

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,18 +12,26 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should redirect edit when logged in as wrong user" do
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
     assert_redirected_to root_url
   end
 
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
 end
